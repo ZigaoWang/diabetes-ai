b4a29568a560e81bb361b9893b5ad0a99a8941cf
Zigao Wang <zigao.wang24@gmail.com>
2025-04-19 12:31:17 +0800

    feat: empty ios project

---
+++ ios/Diabetes AI/Diabetes AI/Diabetes_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diabetes_AIApp.swift
+//  Diabetes AI
+//
+//  Created by Zigao Wang on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Diabetes_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
